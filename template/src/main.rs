451e9a79a7861bf8751e51e4fc4e9449eff5d47e
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2022-12-06 18:07:52 +0100

    feat: adding template

---
+++ template/src/main.rs
@@ -0,0 +1,15 @@
+use anyhow::{Context, Error, Result};
+
+fn part1(input: &str) -> Result<()> {
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let input = include_str!("../input");
+
+    Ok(())
+}
