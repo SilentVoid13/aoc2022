544bfcf251eb097902cb2c4b8b487e4f712843a3
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2023-09-07 15:42:03 +0200

    refactor: updating template with std::Instant

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,15 +1,28 @@
+use std::time::Instant;
+
 use anyhow::{Context, Error, Result};
 
-fn part1(input: &str) -> Result<()> {
-    Ok(())
+fn part1(input: &str) -> Result<usize> {
+    Ok(0)
 }
 
-fn part2(input: &str) -> Result<()> {
-    Ok(())
+fn part2(input: &str) -> Result<usize> {
+    Ok(0)
 }
 
 fn main() -> Result<()> {
     let input = include_str!("../input");
 
+    let instant = Instant::now();
+    let res = part1(input)?;
+    let time = Instant::now() - instant;
+    println!("[*] part 1: {} ({:?})", res, time);
+
+    let instant = Instant::now();
+    let res = part2(input)?;
+    let time = Instant::now() - instant;
+    println!("[*] part 2: {} ({:?})", res, time);
+
     Ok(())
 }
+
